78b60da131d932c74ca9d5859229a3a4518de674
Abrahamkivosh <abrahamkivosh@gmail.com>
2019-02-11 00:25:13 +0300

    Removed my gmail password for security measures

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -8,33 +8,33 @@ class MailController extends Controller
 {
     public function basic_email()
     {
-        $data = array('name' => 'Virat Gandhi');
+        $data = array('name' => 'Abraham Kivosh');
 
         Mail::send(['text' => 'mail'], $data, function ($message) {
-            $message->to('abc@gmail.com', 'Tutorials Point')->subject('Laravel Basic Testing Mail');
-            $message->from('xyz@gmail.com', 'Virat Gandhi');
+            $message->to('abrahamkivosh@tbxhost.co.uk', 'Abraham Kivosh')->subject('Laravel Basic Testing Mail');
+            $message->from('abrahamkivosh@gmail.com', 'Abraham Kivosh');
         });
         echo 'Basic Email Sent. Check your inbox.';
     }
 
     public function html_email()
     {
-        $data = array('name' => 'Virat Gandhi');
+        $data = array('name' => 'Abraham Kivondo');
         Mail::send('mail', $data, function ($message) {
-            $message->to('abc@gmail.com', 'Tutorials Point')->subject('Laravel HTML Testing Mail');
-            $message->from('xyz@gmail.com', 'Virat Gandhi');
+            $message->to('abrahamkivosh@tbxhost.co.uk', 'mnoma')->subject('Coding is the best thing');
+            $message->from('abraham.kivondo@students.jkuat.ac.ke', 'abraham.kivondo');
         });
         echo 'HTML Email Sent. Check your inbox.';
     }
 
     public function attachment_email()
     {
-        $data = array('name' => 'Virat Gandhi');
+        $data = array('name' => 'Abraham kivondo');
         Mail::send('mail', $data, function ($message) {
-            $message->to('abc@gmail.com', 'Tutorials Point')->subject('Laravel Testing Mail with Attachment');
-            $message->attach('C:\laravel-master\laravel\public\uploads\image.png');
-            $message->attach('C:\laravel-master\laravel\public\uploads\test.txt');
-            $message->from('xyz@gmail.com', 'Virat Gandhi');
+            $message->to('abrahamkivosh@gmail.com', 'Code mafia')->subject('Laravel Testing Mail with Attachment');
+            $message->attach('C:\Users\yesterday\Desktop\no-image-available.png');
+            // $message->attach('C:\laravel-master\laravel\public\uploads\test.txt');
+            $message->from('informationtechnologyjkuat@gmail.com', 'Information Technology');
         });
         echo 'Email Sent with attachment. Check your inbox.';
     }
--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,2 +1,9 @@
-<h1>Hi, {{ $name }}</h1>
-l<p>Sending Mail from Laravel.</p>
+@extends('layouts.app')
+@section('format')
+<div class="container-fluid">
+        <h1 class=" header card-header-tabs">Hi, {{ $name }}</h1>
+        <p class=" border-primary">This massage is sent  using laravel</p>
+</div>
+
+@endsection
+
